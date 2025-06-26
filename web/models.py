989c41c9346a151e3708729f815dfd3622f2917d
Pooyan Hejazi Mahabadi <pooyan.hejazimahabadi@bvtech.com>
2025-06-26 23:53:32 +0200

    Income model has been added

--- web/models.py
+++ web/models.py
@@ -7,3 +7,9 @@ class Expense(models.Model):
     date = models.DateField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
